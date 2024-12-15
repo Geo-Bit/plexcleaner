a8b0fd6bcb0645e773871999e66b976ff9207a83
Geo <gtip22@gmail.com>
2024-12-14 21:35:02 -0700

    debugging

--- scripts/config.py
+++ scripts/config.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    def __init__(self):
-        self.tautulli_url = os.getenv('TAUTULLI_URL')
-        self.api_key = os.getenv('TAUTULLI_API_KEY')
-        self.days_since_watched = int(os.getenv('DAYS_SINCE_WATCHED', 30))
-        self.unwatched_cutoff = int(os.getenv('UNWATCHED_CUTOFF', 180))
-        self.tv_directory = os.getenv('TV_DIRECTORY', '/media/TV')
+    TAUTULLI_URL = os.getenv('TAUTULLI_URL')
+    TAUTULLI_API_KEY = os.getenv('TAUTULLI_API_KEY')
+    DAYS_SINCE_WATCHED = int(os.getenv('DAYS_SINCE_WATCHED', 30))
+    UNWATCHED_CUTOFF = int(os.getenv('UNWATCHED_CUTOFF', 180))
+    TV_DIRECTORY = os.getenv('TV_DIRECTORY', '/media/TV')
